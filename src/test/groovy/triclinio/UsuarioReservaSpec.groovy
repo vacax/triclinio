b17a3386964e33123c4e5210242d96298b5eb63e
dewyn <dewyn.liriano@gmail.com>
2018-10-11 01:11:39 -0400

    Creando relacion usaurio-reservacion

---
+++ src/test/groovy/triclinio/UsuarioReservaSpec.groovy
@@ -0,0 +1,19 @@
+package triclinio
+
+import com.triclinio.domains.restaurante.UsuarioReserva
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class UsuarioReservaSpec extends Specification implements DomainUnitTest<UsuarioReserva> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
